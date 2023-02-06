ddc3b20c673559b31da7cafb0ff0270e3445a548
Rômulo da Silva Cavalcanti <20221014040015@ifrn.local>
2023-02-06 15:56:06 -0300

    finishing Q10

--- exercise-lists/l1/Q10.c
+++ exercise-lists/l1/Q10.c
@@ -15,15 +15,40 @@ int c(int n) {
 }
 
 int d(int n) {
-    return 100 + 20*log(2);
+    return 100 + 20*log(2)*n;
 }
+
+
 
 int main() {
     int i, x;
-    printf("Input | A | B | C | D\n");
+    /*
+    printf("Input\\ A \\ B  \\ C  \\ D\n");
     for (i=2; i<21; i=i+2){
         printf("i = %d:  %d \\ %d \\ %d \\ %d\n", i, a(i), b(i), c(i), d(i));
     }
+    */
+    printf("alg A: ");
+    for(i=2;i<21;i=i+2) {
+        printf("%7d ", a(i));
+    }
+    printf("\n");
+    printf("alg B: ");
+    for(i=2;i<21;i=i+2) {
+        printf("%7d ", b(i));
+    }
+    printf("\n");
+    printf("alg C: ");
+    for(i=2;i<21;i=i+2) {
+        printf("%7d ", c(i));
+    }
+    printf("\n");
+    printf("alg D: ");
+    for(i=2;i<21;i=i+2) {
+        printf("%7d ", d(i));
+    }
+    printf("\n");
+
 
     return 0;
 }
