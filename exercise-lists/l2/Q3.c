69cb7083197fbaf037d87b93c6ca4fcfa62bcd8d
Rômulo da Silva <roomulo.cavalkant@outlook.com>
2023-02-08 22:14:34 +0000

    now Q3 works, but wrong

--- exercise-lists/l2/Q3.c
+++ exercise-lists/l2/Q3.c
@@ -3,7 +3,6 @@
 long double massloss(long long int t, long double mi) {
     long double mass = mi;
     for(t=0; 1<10; t++){
-        printf("%f ", mass);
         mass = mass/2;
         if (mass < 1){
             return t;
@@ -12,12 +11,13 @@ long double massloss(long long int t, long double mi) {
 }
 
 int main() {
-    long double t, mi;
+    long double mi;
+    int t;
 
     printf("escreva o tempo:\n");
     scanf("%d", &t);
     printf("agora a massa inicial:\n");
-    scanf("%f", &mi);
-
-    printf("%d", massloss(t, mi));
+    scanf("%Lf", &mi);
+    long double x = massloss(t, mi);
+    printf("%.2Lf segundos", x);
 }
