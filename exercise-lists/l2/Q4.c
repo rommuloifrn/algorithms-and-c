fa777b85cf7d06de10fffec9696e79c6fae5a925
Rômulo da Silva <roomulo.cavalkant@outlook.com>
2023-02-10 20:55:56 +0000

    l2 Q4 done

--- exercise-lists/l2/Q4.c
+++ exercise-lists/l2/Q4.c
@@ -1,27 +1,52 @@
 #include <stdio.h>
 
-void there_rep(int a[], int b[], int capacidade) {
+void zero(int *x) {
     int i;
-    for(i=0; i<capacidade; i++){
-        b[a[i]]++;
-        if (b[a[i]] > 1)
-            printf("tem um número repetido, e é o %d", b[a[i]]);
-            break;
+    for(i=0; i<1000000; i++){
+        x[i] = 0;
     }
-    printf("Não há nenhum número repetido.");
+}
+
+void runthrough(int x[], int limite) {
+    int i;
+    printf("> 0 a %d: { ", limite-1);
+    for(i=0; i<limite; i++) {
+        printf("%d ", x[i]);
+    }
+    printf("}\n\n");
+}
+// Função que confere a primeira ocasião de números repetidos em um array, ignorando os zeros.
+// recebe o array a ser conferido e um array só com zeros.
+void there_rep(int a[], int b[], int capacidade) {
+    int i, x;
+    // para cada elemento
+    for(i=0; i<capacidade; i++){
+
+        if (a[i] != 0) {
+            b[a[i]]++;
+        }
+        
+        if (b[a[i]] > 1) {
+            printf("Há um número repetido: é o %d\n", a[i]);
+            return;
+        }
+
+    }
+    printf("Não há elementos repetidos\n");
+    return;
 }
 
 int main() {
     int i;
 
-    int a[1000000] = {1, 2, 3, 5, 7, 28, 34, 19, 20, 19, 32, 32};
+    int a[1000000] = {24, 279, 3, 5, 7, 19, 34, 19, 20, 19, 32, 32};
     int b[1000000];
 
-    // função pra zerar o b[0]
-    for(i=0; i<1000000; i++){
-        b[i] = 0;
-    }
+    zero(b);
+
     there_rep(a, b, 1000000 );
+    runthrough(a, 100);
+    runthrough(b, 100);
     
 
     return 0;
