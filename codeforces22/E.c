9214af588b3ed45dde1f83d4f02ac0c94b43b471
Rômulo da Silva <roomulo.cavalkant@outlook.com>
2023-04-19 16:19:46 +0000

    E done

--- codeforces22/E.c
+++ codeforces22/E.c
@@ -3,25 +3,43 @@
 int main() {
     while (1) {
         int samples, peaks=0;
-        int wave[samples];
         scanf("%d", &samples);
+        int wave[samples];
         if (samples == 0) return 0;
 
         for (int i=0; i<samples; i++) {
             scanf("%d", &wave[i]);    
         }
-            
+        /*
         // 1 = up, 0 = down
         int direction = 2;
-        for (int i=0; i<samples-1; i++) {
+        for (int i=0; i<samples; i++) {
+            printf("i = %d\n", i);
+            // caso chegue no último elemento
+            if (i==samples-1) {
+                if (direction == 1 && wave[0]<wave[i]) peaks++; else
+                if (direction == 0 && wave[0]>wave[i]) peaks++;
+            }
             if (direction == 1 && wave[i+1]<wave[i]) peaks++; else
             if (direction == 0 && wave[i+1]>wave[i]) peaks++;
+            
 
             if (wave[i+1]> wave[i]) direction = 1;
             else if (wave[i+1]<wave[i]) direction = 0;
         } 
+        */
+        int next, previous;
+        for (int i=0; i<samples; i++) {
+            if (i==samples-1) next = 0; else next = i+1;
+            if (i==0) previous = samples-1; else previous = i-1;
 
-        printf("%d", peaks);
+            if (wave[i]<wave[previous])
+                if(wave[i]<wave[next]) peaks++;
+            if (wave[i]>wave[previous])
+                if(wave[i]>wave[next]) peaks++;
+        } 
+
+        printf("%d\n", peaks);
 
         
 
